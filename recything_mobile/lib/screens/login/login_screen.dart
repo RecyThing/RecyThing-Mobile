d411376de492486c7e44ffcc8b776d249dee9f1a
madvier83 <madvier83@gmail.com>
2023-11-06 22:07:10 +0700

    add login screen

---
+++ recything_mobile/lib/screens/login/login_screen.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:recything_mobile/constants/pallete.dart';
+import 'package:recything_mobile/widgets/forms/google_button.dart';
+import 'package:recything_mobile/widgets/forms/main_button.dart';
+import 'package:recything_mobile/widgets/forms/main_textfield.dart';
+import 'package:recything_mobile/widgets/typography/body_link.dart';
+import 'package:recything_mobile/widgets/typography/body_normal.dart';
+import 'package:recything_mobile/widgets/typography/heading3.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 80,
+              ),
+              const Heading3("Masuk"),
+              const SizedBox(
+                height: 8,
+              ),
+              const SizedBox(
+                width: 242,
+                child: BodyNormal(
+                  "Masuk akun Anda sekarang dan mulailah menjelajah",
+                ),
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              const MainTextField(
+                  label: "Masukan Email / No HP",
+                  prefixIcon: Icons.mail_outline_rounded),
+              const SizedBox(
+                height: 12,
+              ),
+              const MainTextField(
+                  label: "Masukan Kata Sandi",
+                  prefixIcon: Icons.lock_outline_rounded),
+              const SizedBox(
+                height: 12,
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  BodyNormal("Lupa Password? "),
+                  BodyLink("Atur Ulang"),
+                ],
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                      child: MainButton(
+                    text: "Login",
+                    onPressed: () {},
+                  )),
+                ],
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                      child: GoogleButton(
+                    text: "Masuk Dengan Google",
+                    onPressed: () {},
+                  )),
+                ],
+              ),
+              const Expanded(child: SizedBox()),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  BodyNormal("Tidak Punya Akun? "),
+                  BodyLink("Register"),
+                ],
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
