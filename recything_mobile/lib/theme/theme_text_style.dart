3bf4642efc91ecc898c0554f8320563f4a5f05bc
herliananur <herliananur19@gmail.com>
2023-11-10 17:07:56 +0800

    theme color dan text style

---
+++ recything_mobile/lib/theme/theme_text_style.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class ThemeTextStyle {
+  TextStyle display1 = GoogleFonts.inter(
+    fontSize: 48,
+    fontWeight: FontWeight.w500,
+  );
+  TextStyle display2 = GoogleFonts.inter(
+    fontSize: 40,
+    fontWeight: FontWeight.w300,
+  );
+  TextStyle heading1 = GoogleFonts.inter(
+    fontSize: 44,
+    fontWeight: FontWeight.w700,
+  );
+  TextStyle heading2 = GoogleFonts.inter(
+    fontSize: 36,
+    fontWeight: FontWeight.w700,
+  );
+  TextStyle heading3 = GoogleFonts.inter(
+    fontSize: 28,
+    fontWeight: FontWeight.w700,
+  );
+  TextStyle heading4 = GoogleFonts.inter(
+    fontSize: 24,
+    fontWeight: FontWeight.w700,
+  );
+  TextStyle heading5 = GoogleFonts.inter(
+    fontSize: 20,
+    fontWeight: FontWeight.w700,
+  );
+  TextStyle heading6 = GoogleFonts.inter(
+    fontSize: 16,
+    fontWeight: FontWeight.w700,
+  );
+  TextStyle leadParagraph = GoogleFonts.inter(
+    fontSize: 22,
+    fontWeight: FontWeight.w400,
+  );
+  TextStyle bodyLarge = GoogleFonts.inter(
+    fontSize: 20,
+    fontWeight: FontWeight.w400,
+  );
+  TextStyle leadParagraph = GoogleFonts.inter(
+    fontSize: 22,
+    fontWeight: FontWeight.w400,
+  );
+  TextStyle leadParagraph = GoogleFonts.inter(
+    fontSize: 22,
+    fontWeight: FontWeight.w400,
+  );
+  TextStyle leadParagraph = GoogleFonts.inter(
+    fontSize: 22,
+    fontWeight: FontWeight.w400,
+  );
+}
