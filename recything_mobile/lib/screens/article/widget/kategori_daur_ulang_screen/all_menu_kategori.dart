e186a5b78a6c6f6c8ebdb2fe17e67c1645275f25
sintaayr19 <sintaayr03@gmail.com>
2023-11-10 14:07:00 +0700

    update icon pada halaman kategori daur ulang

--- recything_mobile/lib/screens/article/widget/kategori_daur_ulang_screen/all_menu_kategori.dart
+++ recything_mobile/lib/screens/article/widget/kategori_daur_ulang_screen/all_menu_kategori.dart
@@ -7,47 +7,47 @@ class AllMenuKategoriWidget extends StatelessWidget {
   // Data list map
   final List<Map<String, String>> macamKategori = [
     {
-      'imageAsset': 'assets/icons/icon_plastik_noBg.png',
+      'imageAsset': 'assets/icons/icon_plastik_withText.png',
       'title': 'Plastik',
     },
     {
-      'imageAsset': 'assets/icons/icon_kaca_noBg.png',
+      'imageAsset': 'assets/icons/icon_kaca_withText.png',
       'title': 'Kaca',
     },
     {
-      'imageAsset': 'assets/icons/icon_logam_noBg.png',
+      'imageAsset': 'assets/icons/icon_logam_withText.png',
       'title': 'Logam',
     },
     {
-      'imageAsset': 'assets/icons/icon_organik_noBg.png',
+      'imageAsset': 'assets/icons/icon_organik_withText.png',
       'title': 'Organik',
     },
     {
-      'imageAsset': 'assets/icons/icon_kertas_noBg.png',
+      'imageAsset': 'assets/icons/icon_kertas_withText.png',
       'title': 'Kertas',
     },
     {
-      'imageAsset': 'assets/icons/icon_kaleng_noBg.png',
+      'imageAsset': 'assets/icons/icon_kaleng_withText.png',
       'title': 'Kaleng',
     },
     {
-      'imageAsset': 'assets/icons/icon_minyak_noBg.png',
+      'imageAsset': 'assets/icons/icon_minyak_withText.png',
       'title': 'Minyak',
     },
     {
-      'imageAsset': 'assets/icons/icon_elektronik_noBg.png',
+      'imageAsset': 'assets/icons/icon_elektronik_withText.png',
       'title': 'Elektronik',
     },
     {
-      'imageAsset': 'assets/icons/icon_tekstil_noBg.png',
+      'imageAsset': 'assets/icons/icon_tekstil_withText.png',
       'title': 'Tekstil',
     },
     {
-      'imageAsset': 'assets/icons/icon_pakaian_noBg.png',
+      'imageAsset': 'assets/icons/icon_pakaian_withText.png',
       'title': 'Pakaian',
     },
     {
-      'imageAsset': 'assets/icons/icon_baterai_noBg.png',
+      'imageAsset': 'assets/icons/icon_baterai_withText.png',
       'title': 'Baterai',
     },
   ];
@@ -75,25 +75,20 @@ class AllMenuKategoriWidget extends StatelessWidget {
                 height: 99.58,
                 child: Column(
                   children: [
-                    Container(
-                      decoration: const BoxDecoration(),
-                      height: 69.77,
-                      width: 69.77,
-                      child: Image.asset(
-                        macamKategori[index]['imageAsset']!,
-                        fit: BoxFit.cover,
-                      ),
+                    Image.asset(
+                      macamKategori[index]['imageAsset']!,
+                      fit: BoxFit.cover,
                     ),
-                    const SizedBox(height: 9.66),
-                    SizedBox(
-                      height: 19.66,
-                      width: 98.31,
-                      child: Text(
-                        macamKategori[index]['title']!,
-                        textAlign: TextAlign.center,
-                        style: ThemeText().bodyNormalMedium,
-                      ),
-                    )
+                    // const SizedBox(height: 9.66),
+                    // SizedBox(
+                    //   height: 19.66,
+                    //   width: 98.31,
+                    //   child: Text(
+                    //     macamKategori[index]['title']!,
+                    //     textAlign: TextAlign.center,
+                    //     style: ThemeText().bodyNormalMedium,
+                    //   ),
+                    // )
                   ],
                 ),
               ),
