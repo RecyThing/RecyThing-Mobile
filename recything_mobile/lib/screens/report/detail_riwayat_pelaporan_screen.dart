3ae74e4545a8bce24beb72c6f0a36f62a69b43ce
herliananur <herliananur19@gmail.com>
2023-11-13 21:00:30 +0800

    Update detail_riwayat_pelaporan_screen.dart

--- recything_mobile/lib/screens/report/detail_riwayat_pelaporan_screen.dart
+++ recything_mobile/lib/screens/report/detail_riwayat_pelaporan_screen.dart
@@ -1,15 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:recything_mobile/constants/pallete.dart';
+import 'package:recything_mobile/screens/report/widget/custom_card.dart';
 
 class DetailRiwayatPelaporanScreen extends StatefulWidget {
   const DetailRiwayatPelaporanScreen({super.key});
 
   @override
-  State<DetailRiwayatPelaporanScreen> createState() => _DetailRiwayatPelaporanScreenState();
+  State<DetailRiwayatPelaporanScreen> createState() =>
+      _DetailRiwayatPelaporanScreenState();
 }
 
-class _DetailRiwayatPelaporanScreenState extends State<DetailRiwayatPelaporanScreen> {
+class _DetailRiwayatPelaporanScreenState
+    extends State<DetailRiwayatPelaporanScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Detail pelaporan'),
+        centerTitle: true,
+      ),
+      body: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        CustomCard(
+            title: title,
+            description: description,
+            assetImage: assetImage,
+            cardColor: cardColor)
+      ]),
+    );
   }
 }
