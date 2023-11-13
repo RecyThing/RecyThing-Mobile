e28194d64199a33114b145ccd459c3e8a8cc9f98
herliananur <herliananur19@gmail.com>
2023-11-13 20:43:32 +0800

    add detail riwayat pelaporan

---
+++ recything_mobile/lib/screens/report/detail_riwayat_pelaporan_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DetailRiwayatPelaporanScreen extends StatefulWidget {
+  const DetailRiwayatPelaporanScreen({super.key});
+
+  @override
+  State<DetailRiwayatPelaporanScreen> createState() => _DetailRiwayatPelaporanScreenState();
+}
+
+class _DetailRiwayatPelaporanScreenState extends State<DetailRiwayatPelaporanScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
