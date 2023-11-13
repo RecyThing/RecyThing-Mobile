8ca253fea9e92f91047dd6a917529d3767527077
herliananur <herliananur19@gmail.com>
2023-11-13 20:59:50 +0800

    fixed custom card

--- recything_mobile/lib/screens/report/choose_report_screen.dart
+++ recything_mobile/lib/screens/report/choose_report_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:recything_mobile/constants/pallete.dart';
+import 'package:recything_mobile/screens/report/widget/custom_card.dart';
 
 class ChooseReportScreen extends StatefulWidget {
   const ChooseReportScreen({Key? key}) : super(key: key);
@@ -32,27 +33,37 @@ class _ChooseReportScreenState extends State<ChooseReportScreen> {
             ),
           ),
           GestureDetector(
-            onTap: () {
-              Navigator.pushNamed(context, '/report-rubbish');
-            },
-            child: buildCard(
-              'Tumpukan Sampah',
-              'Lapor penumpukan sampah sembarangan.',
-              'assets/images/tumpukan_sampah.png',
-              const Color(0XFFE2FEE1),
-            ),
-          ),
+              onTap: () {
+                Navigator.pushNamed(context, '/report-rubbish');
+              },
+              child: CustomCard(
+                  title: 'Tumpukan Sampah',
+                  description: 'Lapor penumpukan sampah sembarangan',
+                  assetImage: 'assets/images/tumpukan_sampah.png',
+                  cardColor:  const Color(0XFFE2FEE1))
+              // buildCard(
+              //   'Tumpukan Sampah',
+              //   'Lapor penumpukan sampah sembarangan.',
+              //   'assets/images/tumpukan_sampah.png',
+              //   const Color(0XFFE2FEE1),
+              // ),
+              ),
           GestureDetector(
-            onTap: () {
-              // Handle card tap action
-            },
-            child: buildCard(
-              'Pelanggaran Sampah',
-              'Lapor aksi pembuangan sampah sembarangan.',
-              'assets/images/pelanggaran_sampah.png',
-              const Color(0XFFFEF7E0),
-            ),
-          ),
+              onTap: () {
+                // Handle card tap action
+              },
+              child: CustomCard(
+                  title: 'Pelanggaran Sampah',
+                  description: 'Lapor aksi pembuangan sampah sembarangan',
+                  assetImage: 'assets/images/pelanggaran_sampah.png',
+                  cardColor: const Color(0XFFFEF7E0))
+              // buildCard(
+              //   'Pelanggaran Sampah',
+              //   'Lapor aksi pembuangan sampah sembarangan.',
+              //   'assets/images/pelanggaran_sampah.png',
+              //   const Color(0XFFFEF7E0),
+              // ),
+              ),
           const SizedBox(
             height: 32,
           ),
@@ -95,76 +106,76 @@ class _ChooseReportScreenState extends State<ChooseReportScreen> {
     );
   }
 
-  Widget buildCard(
-    String title,
-    String description,
-    String assetImage,
-    Color cardColor,
-  ) {
-    return Padding(
-      padding: const EdgeInsets.all(16),
-      child: Material(
-        elevation: 4,
-        borderRadius: BorderRadius.circular(12),
-        color: Colors.white,
-        child: Row(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(16),
-              child: Container(
-                height: 88,
-                width: 88,
-                padding: const EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                  color: cardColor,
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                child: Image.asset(
-                  assetImage,
-                  fit: BoxFit.contain,
-                ),
-              ),
-            ),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    title,
-                    style: ThemeFont.bodyNormal,
-                  ),
-                  const SizedBox(
-                    height: 8,
-                  ),
-                  Text(
-                    description,
-                    style: TextStyle(
-                      color: Pallete.dark3,
-                      fontSize: ThemeFont.bodySmall.fontSize,
-                      fontWeight: ThemeFont.bodySmall.fontWeight,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 48, bottom: 48, right: 16),
-              child: Container(
-                width: 35,
-                height: 35,
-                decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Pallete.mainDarker,
-                ),
-                child: const Icon(
-                  Icons.arrow_forward_ios,
-                  color: Colors.white,
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+  // Widget buildCard(
+  //   String title,
+  //   String description,
+  //   String assetImage,
+  //   Color cardColor,
+  // ) {
+  //   return Padding(
+  //     padding: const EdgeInsets.all(16),
+  //     child: Material(
+  //       elevation: 4,
+  //       borderRadius: BorderRadius.circular(12),
+  //       color: Colors.white,
+  //       child: Row(
+  //         children: [
+  //           Padding(
+  //             padding: const EdgeInsets.all(16),
+  //             child: Container(
+  //               height: 88,
+  //               width: 88,
+  //               padding: const EdgeInsets.all(20),
+  //               decoration: BoxDecoration(
+  //                 color: cardColor,
+  //                 borderRadius: BorderRadius.circular(12),
+  //               ),
+  //               child: Image.asset(
+  //                 assetImage,
+  //                 fit: BoxFit.contain,
+  //               ),
+  //             ),
+  //           ),
+  //           Expanded(
+  //             child: Column(
+  //               crossAxisAlignment: CrossAxisAlignment.start,
+  //               children: [
+  //                 Text(
+  //                   title,
+  //                   style: ThemeFont.bodyNormal,
+  //                 ),
+  //                 const SizedBox(
+  //                   height: 8,
+  //                 ),
+  //                 Text(
+  //                   description,
+  //                   style: TextStyle(
+  //                     color: Pallete.dark3,
+  //                     fontSize: ThemeFont.bodySmall.fontSize,
+  //                     fontWeight: ThemeFont.bodySmall.fontWeight,
+  //                   ),
+  //                 ),
+  //               ],
+  //             ),
+  //           ),
+  //           Padding(
+  //             padding: const EdgeInsets.only(top: 48, bottom: 48, right: 16),
+  //             child: Container(
+  //               width: 35,
+  //               height: 35,
+  //               decoration: const BoxDecoration(
+  //                 shape: BoxShape.circle,
+  //                 color: Pallete.mainDarker,
+  //               ),
+  //               child: const Icon(
+  //                 Icons.arrow_forward_ios,
+  //                 color: Colors.white,
+  //               ),
+  //             ),
+  //           ),
+  //         ],
+  //       ),
+  //     ),
+  //   );
+  // }
 }
