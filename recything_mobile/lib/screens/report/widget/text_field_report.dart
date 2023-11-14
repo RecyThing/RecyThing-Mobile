e4d50aad718aecec48864f54f9e30e9ee9efd8f4
herliananur <herliananur19@gmail.com>
2023-11-14 23:43:25 +0800

    Create text_field_report.dart

---
+++ recything_mobile/lib/screens/report/widget/text_field_report.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:recything_mobile/constants/pallete.dart';
+
+class TextFieldReport extends StatelessWidget {
+  final String hinttext;
+  final int? maxLines;
+  final Color? focusColor;
+  final TextStyle? labelStyle;
+  final EdgeInsetsGeometry? contentPadding;
+
+  const TextFieldReport({
+    Key? key,
+    required this.hinttext,
+    this.maxLines,
+    this.focusColor,
+    this.labelStyle,
+    this.contentPadding,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide(color: focusColor ?? Pallete.main),
+        ),
+        hintText: hinttext,
+        hintStyle: labelStyle ?? const TextStyle(color: Pallete.dark3),
+        contentPadding: contentPadding ?? const EdgeInsets.all(16),
+      ),
+      style: const TextStyle(
+        backgroundColor: Colors.white,
+      ),
+      maxLines: maxLines,
+    );
+  }
+}
