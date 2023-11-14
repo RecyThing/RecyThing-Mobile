6322793cfb0be640d8014580e1abee8ced36886f
herliananur <herliananur19@gmail.com>
2023-11-14 18:45:22 +0800

    adding fitur permission location

--- recything_mobile/lib/screens/report/report_rubbish_maps_screen.dart
+++ recything_mobile/lib/screens/report/report_rubbish_maps_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class ReportRubbishMapsScreen extends StatefulWidget {
   const ReportRubbishMapsScreen({super.key});
@@ -13,74 +14,126 @@ class _ReportRubbishMapsScreenState extends State<ReportRubbishMapsScreen> {
   Position? _currentPosition;
 
   Future<void> _getCurrentPosition() async {
-    final hasPermission = await _handleLocationPermission(context);
+    PermissionStatus status = await _handleLocationPermission(context);
 
-    if (!hasPermission) return;
-    await Geolocator.getCurrentPosition(
-      desiredAccuracy: LocationAccuracy.high,
-    ).then(
-      (Position position) {
-        setState(
-          () {
-            _currentPosition = position;
-          },
+    if (status == PermissionStatus.granted) {
+      try {
+        Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.high,
         );
-      },
-    );
+        setState(() {
+          _currentPosition = position;
+        });
+      } catch (e) {
+        print('Error getting location: $e');
+      }
+    }
   }
 
-  Future<bool> _handleLocationPermission(BuildContext context) async {
-    bool serviceEnabled;
-    LocationPermission locationPermission;
+  Future<PermissionStatus> _handleLocationPermission(
+      BuildContext context) async {
+    PermissionStatus status = await Permission.location.request();
 
-    // cek apakah gps aktif atau tidak
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    print(serviceEnabled);
-
-    if (!serviceEnabled) {
-      if (context.mounted) {
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(
-            content: Text(
-              'GPS tidak aktif, silahkan aktifkan terlebih dahulu',
-            ),
-          ),
-        );
-      }
-      return false;
+    if (status == PermissionStatus.denied) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(
+              'Izin lokasi ditolak. Aktifkan izin di pengaturan aplikasi.'),
+        ),
+      );
+    } else if (status == PermissionStatus.permanentlyDenied) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(
+              'Izin lokasi ditolak secara permanen. Buka pengaturan aplikasi untuk mengaktifkan izin.'),
+        ),
+      );
+    } else if (status == PermissionStatus.granted) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Izin lokasi diberikan.'),
+        ),
+      );
     }
 
-    locationPermission = await Geolocator.checkPermission();
-    print(locationPermission);
-
-    if (locationPermission == LocationPermission.denied) {
-      if (context.mounted) {
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(
-            content: Text(
-              'Anda belum mengaktifkan lokasi di aplikasi anda',
-            ),
-          ),
-        );
-      }
-      return false;
-    }
-
-    if (locationPermission == LocationPermission.deniedForever) {
-      if (context.mounted) {
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(
-            content: Text(
-              'Anda perlu mengaktifkan izin lewat izin hp anda',
-            ),
-          ),
-        );
-      }
-      return false;
-    }
-
-    return true;
+    return status;
   }
+
+  // Future<void> _getCurrentPosition() async {
+  //   final hasPermission = await _handleLocationPermission(context);
+
+  //   if (!hasPermission) return;
+  //   await Geolocator.getCurrentPosition(
+  //     desiredAccuracy: LocationAccuracy.high,
+  //   ).then(
+  //     (Position position) {
+  //       setState(
+  //         () {
+  //           _currentPosition = position;
+  //         },
+  //       );
+  //     },
+  //   );
+  // }
+
+  // Future<bool> _handleLocationPermission(BuildContext context) async {
+  //   bool serviceEnabled;
+  //   LocationPermission locationPermission;
+
+  //   // cek apakah gps aktif atau tidak
+  //   serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  //   print(serviceEnabled);
+
+  //   if (!serviceEnabled) {
+  //     if (context.mounted) {
+  //       ScaffoldMessenger.of(context).showSnackBar(
+  //         const SnackBar(
+  //           content: Text(
+  //             'GPS tidak aktif, silahkan aktifkan terlebih dahulu',
+  //           ),
+  //         ),
+  //       );
+  //     }
+  //     return false;
+  //   }
+
+  //   locationPermission = await Geolocator.checkPermission();
+  //   print(locationPermission);
+
+  //   if (locationPermission == LocationPermission.denied) {
+  //     if (context.mounted) {
+  //       ScaffoldMessenger.of(context).showSnackBar(
+  //         const SnackBar(
+  //           content: Text(
+  //             'Anda belum mengaktifkan izin lokasi di aplikasi anda',
+  //           ),
+  //         ),
+  //       );
+  //     }
+  //     return false;
+  //   }
+
+  //   if (locationPermission == LocationPermission.deniedForever) {
+  //     if (context.mounted) {
+  //       ScaffoldMessenger.of(context).showSnackBar(
+  //         const SnackBar(
+  //           content: Text(
+  //             'Anda perlu mengaktifkan izin lewat izin hp anda',
+  //           ),
+  //         ),
+  //       );
+  //     }
+  //     return false;
+  //   }
+
+  //   return true;
+  // }
+
+  // @override
+  // void initState() {
+  //   requestPermission();
+  //   super.initState();
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -95,8 +148,8 @@ class _ReportRubbishMapsScreenState extends State<ReportRubbishMapsScreen> {
             Text('Latitude: ${_currentPosition?.latitude ?? "-"}'),
             Text('Longitude: ${_currentPosition?.longitude ?? "-"}'),
             ElevatedButton(
-              onPressed: () {
-                _getCurrentPosition();
+              onPressed: () async {
+                await _getCurrentPosition();
               },
               child: Text('Get Location'),
             ),
