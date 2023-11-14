32ae1509d737056dd7d8b640d59d37b915a23435
herliananur <herliananur19@gmail.com>
2023-11-14 23:09:30 +0800

    Update main.dart

--- recything_mobile/lib/main.dart
+++ recything_mobile/lib/main.dart
@@ -41,7 +41,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Pallete.main),
         useMaterial3: true,
       ),
-      initialRoute: '/report-rubbish-maps',
+      initialRoute: '/choose-report',
       routes: {
         // '/splash': (context) => const SplashScreen(),
         // '/onboarding': (context) => const OnboardingScreen(),
