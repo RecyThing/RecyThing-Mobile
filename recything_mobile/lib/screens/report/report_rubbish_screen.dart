de220b238963c8483ffa5d1d48d03efa3bb9f9e8
herliananur <herliananur19@gmail.com>
2023-11-15 22:29:41 +0800

    Update report_rubbish_screen.dart

--- recything_mobile/lib/screens/report/report_rubbish_screen.dart
+++ recything_mobile/lib/screens/report/report_rubbish_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:iconly/iconly.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:recything_mobile/constants/pallete.dart';
+import 'package:recything_mobile/screens/report/detail_riwayat_pelaporan_screen.dart';
 import 'package:recything_mobile/screens/report/report_rubbish_maps_screen.dart';
 import 'package:recything_mobile/screens/report/widget/text_field_report.dart';
 import 'package:recything_mobile/widgets/forms/main_button.dart';
@@ -46,194 +47,204 @@ class _ReportRubbishScreenState extends State<ReportRubbishScreen> {
           ),
           centerTitle: true,
         ),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(16),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  children: [
-                    const Expanded(
-                      child: MainTextField(
-                        // controller: TextEditingController(
-                        //     text: _locationAddress ?? '-'),
-                        label: 'Lokasi Sampah',
-                        prefixIcon: IconlyLight.location,
+        body: Builder(builder: (context) {
+          return SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      const Expanded(
+                        child: MainTextField(
+                          // controller: TextEditingController(
+                          //     text: _locationAddress ?? '-'),
+                          label: 'Lokasi Tumpukan',
+                          prefixIcon: IconlyLight.location,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 12,
+                      ),
+                      SizedBox(
+                        width: 56,
+                        height: 56,
+                        child: ElevatedButton(
+                          onPressed: () async {
+                            final result = await Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) =>
+                                    const ReportRubbishMapsScreen(),
+                              ),
+                            );
+
+                            //callback maps
+                            setState(() {
+                              //set nilai ke controller
+                              _locationAddress = result as String?;
+                            });
+                          },
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Pallete.main,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(12),
+                              )),
+                          child: Image.asset(
+                            'assets/images/location_map.png',
+                            width: 24,
+                            height: 24,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 24,
+                  ),
+                  Text(
+                    'Tambah Lokasi Patokan',
+                    style: TextStyle(
+                        fontSize: ThemeFont.bodySmall.fontSize,
+                        fontWeight: FontWeight.w500),
+                  ),
+                  TextFieldReport(
+                    hinttext: 'Cth: Sebelah Masjid Nawawi',
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  Text(
+                    'Jenis Sampah',
+                    style: TextStyle(
+                        fontSize: ThemeFont.bodyNormalReguler.fontSize,
+                        fontWeight: FontWeight.w400),
+                  ),
+                  Row(
+                    children: [
+                      Checkbox(
+                        value: isCheckedKering,
+                        onChanged: (bool? value) {
+                          setState(() {
+                            isCheckedKering = value!;
+                          });
+                        },
+                        activeColor: Pallete.main,
+                        side: const BorderSide(
+                          color: Colors.grey,
+                          width: 1,
+                        ),
+                      ),
+                      const Text('Sampah Kering')
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Checkbox(
+                        value: isCheckedBasah,
+                        onChanged: (bool? value) {
+                          setState(() {
+                            isCheckedBasah = value!;
+                          });
+                        },
+                        activeColor: Pallete.main,
+                        side: const BorderSide(color: Colors.grey, width: 1),
+                      ),
+                      const Text('Sampah Basah')
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  Text(
+                    'Ceritakan Kondisi Sampah',
+                    style: TextStyle(
+                        fontSize: ThemeFont.bodyNormal.fontSize,
+                        fontWeight: FontWeight.w400),
+                  ),
+                  const SizedBox(
+                    height: 4,
+                  ),
+                  const TextFieldReport(
+                      maxLines: 5,
+                      hinttext:
+                          'Cth: Saya melihat tumpukan sampah yang sangat banyak, sampah sangat bercampur basah dan kering'),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  Text(
+                    'Bukti Foto / Video',
+                    style: TextStyle(
+                        fontSize: ThemeFont.bodyNormal.fontSize,
+                        fontWeight: FontWeight.w400),
+                  ),
+                  if (selectedImages != null && selectedImages!.isNotEmpty)
+                    Column(
+                      children: [
+                        for (final image in selectedImages!)
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 8),
+                            child: Image.file(
+                              File(image.path),
+                              width: 80,
+                              height: 80,
+                            ),
+                          )
+                      ],
+                    ),
+                  ElevatedButton(
+                    onPressed: pickImage,
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: const Size(80, 80),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
                       ),
                     ),
-                    const SizedBox(
-                      width: 12,
+                    child: const Icon(
+                      Icons.add,
+                      color: Colors.grey,
                     ),
-                    SizedBox(
-                      width: 56,
-                      height: 56,
-                      child: ElevatedButton(
-                        onPressed: () async {
-                          final result = await Navigator.push(
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  Text(
+                    'Maksimum file: 20 MB',
+                    style: ThemeFont.bodySmallRegular.copyWith(
+                        color: Pallete.dark3, fontWeight: FontWeight.w400),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  Align(
+                    alignment: Alignment.center,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16),
+                      child: MainButton(
+                        onPressed: () {
+                          Navigator.push(
                             context,
                             MaterialPageRoute(
                               builder: (context) =>
-                                  const ReportRubbishMapsScreen(),
+                                  DetailRiwayatPelaporanScreen(),
                             ),
                           );
-
-                          //callback maps
-                          setState(() {
-                            //set nilai ke controller
-                            _locationAddress = result as String?;
-                          });
                         },
-                        style: ElevatedButton.styleFrom(
-                            backgroundColor: Pallete.main,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(12),
-                            )),
-                        child: Image.asset(
-                          'assets/images/location_map.png',
-                          width: 24,
-                          height: 24,
-                          color: Colors.white,
+                        child: Text(
+                          "Kirim",
+                          style: ThemeFont.heading6Reguler.copyWith(
+                              color: Pallete.textMainButton,
+                              fontWeight: FontWeight.w700),
                         ),
                       ),
                     ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 24,
-                ),
-                Text(
-                  'Tambah Lokasi Patokan',
-                  style: TextStyle(
-                      fontSize: ThemeFont.bodySmall.fontSize,
-                      fontWeight: FontWeight.w500),
-                ),
-                TextFieldReport(
-                  hinttext: 'Cth: Sebelah Masjid Nawawi',
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                Text(
-                  'Jenis Sampah',
-                  style: TextStyle(
-                      fontSize: ThemeFont.bodyNormalReguler.fontSize,
-                      fontWeight: FontWeight.w400),
-                ),
-                Row(
-                  children: [
-                    Checkbox(
-                      value: isCheckedKering,
-                      onChanged: (bool? value) {
-                        setState(() {
-                          isCheckedKering = value!;
-                        });
-                      },
-                      activeColor: Pallete.main,
-                      side: const BorderSide(
-                        color: Colors.grey,
-                        width: 1,
-                      ),
-                    ),
-                    const Text('Sampah Kering')
-                  ],
-                ),
-                Row(
-                  children: [
-                    Checkbox(
-                      value: isCheckedBasah,
-                      onChanged: (bool? value) {
-                        setState(() {
-                          isCheckedBasah = value!;
-                        });
-                      },
-                      activeColor: Pallete.main,
-                      side: const BorderSide(color: Colors.grey, width: 1),
-                    ),
-                    const Text('Sampah Basah')
-                  ],
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                Text(
-                  'Ceritakan Kondisi Sampah',
-                  style: TextStyle(
-                      fontSize: ThemeFont.bodyNormal.fontSize,
-                      fontWeight: FontWeight.w400),
-                ),
-                const SizedBox(
-                  height: 4,
-                ),
-                const TextFieldReport(
-                    maxLines: 5,
-                    hinttext:
-                        'Cth: Saya melihat tumpukan sampah yang sangat banyak, sampah sangat bercampur basah dan kering'),
-                const SizedBox(
-                  height: 16,
-                ),
-                Text(
-                  'Bukti Foto / Video',
-                  style: TextStyle(
-                      fontSize: ThemeFont.bodyNormal.fontSize,
-                      fontWeight: FontWeight.w400),
-                ),
-                if (selectedImages != null && selectedImages!.isNotEmpty)
-                  Column(
-                    children: [
-                      for (final image in selectedImages!)
-                        Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 8),
-                          child: Image.file(
-                            File(image.path),
-                            width: 80,
-                            height: 80,
-                          ),
-                        )
-                    ],
-                  ),
-                ElevatedButton(
-                  onPressed: pickImage,
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: const Size(80, 80),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                  ),
-                  child: const Icon(
-                    Icons.add,
-                    color: Colors.grey,
-                  ),
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                Text(
-                  'Maksimum file: 20 MB',
-                  style: ThemeFont.bodySmallRegular.copyWith(
-                      color: Pallete.dark3, fontWeight: FontWeight.w400),
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                Align(
-                  alignment: Alignment.center,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16),
-                    child: MainButton(
-                      onPressed: () {},
-                      child: Text(
-                        "Kirim",
-                        style: ThemeFont.heading6Reguler.copyWith(
-                            color: Pallete.textMainButton,
-                            fontWeight: FontWeight.w700),
-                      ),
-                    ),
-                  ),
-                )
-              ],
+                  )
+                ],
+              ),
             ),
-          ),
-        ));
+          );
+        }));
   }
 }
