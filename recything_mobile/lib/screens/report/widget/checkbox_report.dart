c067554d3638a1cbe8bd928a5a0498ebb394e704
herliananur <herliananur19@gmail.com>
2023-11-23 18:31:44 +0800

    Update checkbox_report.dart

--- recything_mobile/lib/screens/report/widget/checkbox_report.dart
+++ recything_mobile/lib/screens/report/widget/checkbox_report.dart
@@ -12,6 +12,7 @@ class CheckboxReport extends StatefulWidget {
   }) : super(key: key);
 
   @override
+  // ignore: library_private_types_in_public_api
   _CheckboxReportState createState() => _CheckboxReportState();
 }
 
