c095740bccde74c0f0c809bf6963a1de8ff82e8b
herliananur <herliananur19@gmail.com>
2023-11-23 17:38:04 +0800

    Update time_picker_widget.dart

--- recything_mobile/lib/screens/report/widget/time_picker_widget.dart
+++ recything_mobile/lib/screens/report/widget/time_picker_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:recything_mobile/constants/pallete.dart';
 
 class TimePickerWWidget extends StatefulWidget {
   const TimePickerWWidget({Key? key}) : super(key: key);
@@ -20,11 +21,27 @@ class _TimePickerWWidgetState extends State<TimePickerWWidget> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.all(16),
+      // padding: const EdgeInsets.all(16),
       child: Center(
         child: TextField(
           controller: timeInput,
-          decoration: const InputDecoration(hintText: "00:00"),
+          decoration: const InputDecoration(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(12),
+              ),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(12)),
+              borderSide: BorderSide(color: Pallete.info),
+            ),
+            focusColor: Pallete.info,
+            hintText: '00:00',
+          ),
+          style: ThemeFont.bodySmall.copyWith(
+            color: Pallete.dark3,
+            fontWeight: FontWeight.w500,
+          ),
           readOnly: true,
           onTap: () async {
             TimeOfDay? pickedTime = await showTimePicker(
@@ -44,7 +61,7 @@ class _TimePickerWWidgetState extends State<TimePickerWWidget> {
               );
 
               print(parsedTime);
-              String formattedTime = DateFormat('HH:mm:ss').format(parsedTime);
+              String formattedTime = DateFormat('hh:mm a').format(parsedTime);
 
               setState(() {
                 timeInput.text = formattedTime;
