dd17a61a90febbb373ad12fcc973ee9928c87891
Puspita Kartika Sari <91021761+PuspitaKartika@users.noreply.github.com>
2023-11-23 20:47:55 +0700

    fix bug onboarding

--- recything_mobile/lib/screens/splash/splash_screen.dart
+++ recything_mobile/lib/screens/splash/splash_screen.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -18,12 +17,12 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 
   Future<void> redirect() async {
-    final pref = await SharedPreferences.getInstance();
-    bool onboarding = pref.getBool('onboarding') ?? false;
+    //   final pref = await SharedPreferences.getInstance();
+    //bool onboarding = pref.getBool('onboarding') ?? false;
     String route = "/onboarding";
-    if (onboarding) {
-      route = "/login";
-    }
+    // if (onboarding) {
+    //   route = "/login";
+    // }
     Timer(const Duration(seconds: 3), () {
       Navigator.of(context).pushNamedAndRemoveUntil(route, (route) => false);
     });
