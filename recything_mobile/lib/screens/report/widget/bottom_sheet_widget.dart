973ce9a618f1a1c5919058e548e66dc54e6674a6
herliananur <herliananur19@gmail.com>
2023-11-27 15:28:04 +0800

    Update bottom_sheet_widget.dart

--- recything_mobile/lib/screens/report/widget/bottom_sheet_widget.dart
+++ recything_mobile/lib/screens/report/widget/bottom_sheet_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:recything_mobile/constants/pallete.dart';
 
 class BottomSheetWidget {
   final String title;
@@ -9,26 +10,47 @@ class BottomSheetWidget {
     required this.subtitle,
   });
 
-  void showBottomSheet(BuildContext context) {
+  void customShowModalBottomSheet(BuildContext context) {
     showModalBottomSheet(
-        context: context,
-        builder: (BuildContext context) {
-          return Column(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          decoration: const BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(24),
+              topRight: Radius.circular(24),
+            ),
+          ),
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
             children: [
-              ListTile(
-                title: Text(title),
-                // onTap: () {
-                //   Navigator.pop(context);
-                // },
+              Padding(
+                padding: const EdgeInsets.only(top: 12, bottom: 32),
+                child: Image.asset(
+                  'assets/icons/slider.png',
+                ),
               ),
               ListTile(
-                title: Text(subtitle),
-                // onTap: () {
-                //   Navigator.pop(context);
-                // },
+                title: Text(
+                  title,
+                  style: ThemeFont.bodySmallSemiBold,
+                ),
+              ),
+              ListTile(
+                title: Text(
+                  subtitle,
+                  style: ThemeFont.bodySmallRegular,
+                ),
+              ),
+              const SizedBox(
+                height: 24,
               )
             ],
-          );
-        });
+          ),
+        );
+      },
+    );
   }
 }
