6b8750d2a1ed397b5b88c20a41baf4d27b6df81b
herliananur <herliananur19@gmail.com>
2023-11-30 16:20:00 +0800

    Update report_repo.dart

--- recything_mobile/lib/repositories/report_repo.dart
+++ recything_mobile/lib/repositories/report_repo.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:logger/logger.dart';
+import 'package:recything_mobile/constants/api.dart';
 import 'package:recything_mobile/models/report_model.dart';
 import 'package:recything_mobile/services/base_service.dart';
 
@@ -17,5 +21,43 @@ class ReportRepo extends BaseService {
     final res = await request(context, 'reports/history/$id');
     Logger().e(res);
     return ReportModel.fromJson(res.data["data"]);
+  }
+
+  Dio dio = Dio(BaseOptions(baseUrl: Api.baseUrl));
+
+  Future<void> reports({
+    required String reportType,
+    required String location,
+    required num longitude,
+    required num latitude,
+    required String addressPoint,
+    required String trashType,
+    required String desc,
+    required List<ImageModel> images,
+  }) async {
+    try {
+      final data = {
+        "report_type": reportType,
+        "location": location,
+        "longitude": longitude,
+        "latitude": latitude,
+        "address_point": addressPoint,
+        "trash_type": trashType,
+        "description": desc,
+        "images": images.map((image) => image.toJson()).toList(),
+      };
+      Logger().i(data);
+
+      final response = await dio.post("reports", data: data);
+
+      Map<String, dynamic> responseData = json.decode(response.toString());
+      Logger().i(responseData);
+    } on DioException catch (e) {
+      Logger().e(e.response);
+      if (e.response != null) {
+        final response = jsonDecode(e.response.toString());
+        throw response["message"];
+      }
+    }
   }
 }
