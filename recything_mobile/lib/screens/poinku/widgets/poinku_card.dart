35b3796d40691f5a448d433895968215c67c2ac7
Puspita Kartika Sari <91021761+PuspitaKartika@users.noreply.github.com>
2023-12-13 19:51:42 +0700

    fix bug #21

--- recything_mobile/lib/screens/poinku/widgets/poinku_card.dart
+++ recything_mobile/lib/screens/poinku/widgets/poinku_card.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:recything_mobile/bloc/get_user_profile/get_user_profile_cubit.dart';
 import 'package:recything_mobile/bloc/post_poin_daily/post_poin_daily_cubit.dart';
 import 'package:recything_mobile/constants/pallete.dart';
-import 'package:recything_mobile/models/user_model.dart';
+import 'package:recything_mobile/screens/poinku/pages/poinku_screen.dart';
 import 'package:recything_mobile/widgets/forms/main_button.dart';
 
 class PoinkuCard extends StatefulWidget {
-  final UserModel user;
-  const PoinkuCard({super.key, required this.user});
+  const PoinkuCard({
+    super.key,
+  });
 
   @override
   State<PoinkuCard> createState() => _PoinkuCardState();
@@ -16,6 +18,10 @@ class PoinkuCard extends StatefulWidget {
 class _PoinkuCardState extends State<PoinkuCard> {
   double dividerWidth = 0;
   int clickCount = 0;
+  void initState() {
+    super.initState();
+    context.read<GetUserProfileCubit>().fetchMe(context);
+  }
 
   void _onTukarPoinClicked(String message) {
     setState(() {
@@ -134,7 +140,10 @@ class _PoinkuCardState extends State<PoinkuCard> {
                     width: MediaQuery.of(context).size.width,
                     child: MainButton(
                         onPressed: () {
-                          Navigator.pop(context);
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => PoinkuScreen()));
                         },
                         child: Text(
                           "Oke",
@@ -151,121 +160,133 @@ class _PoinkuCardState extends State<PoinkuCard> {
   Widget build(BuildContext context) {
     return BlocListener<PostPoinDailyCubit, PostPoinDailyState>(
       listener: (context, state) {
-        if (state is PostPoinDailyFailure)
+        if (state is PostPoinDailyFailure) {
           _onTukarPoinFailure(
               "Kamu sudah claim point' hari ini, Tunggu besok untuk claim selanjutnya");
-        else if (state is PostPoinDailySuccess)
+        } else if (state is PostPoinDailySuccess) {
           _onTukarPoinClicked(state.message);
+        }
       },
-      child: Container(
-        margin: const EdgeInsets.symmetric(horizontal: 16),
-        padding: const EdgeInsets.all(20),
-        width: MediaQuery.of(context).size.width * 0.92,
-        decoration: BoxDecoration(
-            color: Pallete.textMainButton,
-            borderRadius: BorderRadius.circular(12),
-            boxShadow: [
-              BoxShadow(
-                  spreadRadius: 0,
-                  blurRadius: 10,
-                  color: Pallete.dark1.withOpacity(0.15))
-            ]),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              "Point",
-              style: ThemeFont.interText
-                  .copyWith(fontWeight: FontWeight.w600, color: Pallete.dark1),
-            ),
-            const SizedBox(
-              height: 8,
-            ),
-            Text(
-              widget.user.point.toString(),
-              style: ThemeFont.interText
-                  .copyWith(fontWeight: FontWeight.w700, fontSize: 28),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Stack(
-              children: [
-                Positioned(
-                  top: 13,
-                  child: SizedBox(
-                    width: MediaQuery.of(context).size.width,
-                    child: const Divider(
-                      thickness: 4,
-                    ),
+      child: BlocBuilder<GetUserProfileCubit, GetUserProfileState>(
+        builder: (context, state) {
+          if (state is GetUserProfileLoading) {
+            return CircularProgressIndicator();
+          } else if (state is GetUserProfilefailure) {
+            return Text(state.message);
+          } else if (state is GetUserProfileSuccess) {
+            return Container(
+              margin: const EdgeInsets.symmetric(horizontal: 16),
+              padding: const EdgeInsets.all(20),
+              width: MediaQuery.of(context).size.width * 0.92,
+              decoration: BoxDecoration(
+                  color: Pallete.textMainButton,
+                  borderRadius: BorderRadius.circular(12),
+                  boxShadow: [
+                    BoxShadow(
+                        spreadRadius: 0,
+                        blurRadius: 10,
+                        color: Pallete.dark1.withOpacity(0.15))
+                  ]),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Point",
+                    style: ThemeFont.interText.copyWith(
+                        fontWeight: FontWeight.w600, color: Pallete.dark1),
                   ),
-                ),
-                Positioned(
-                  top: 13,
-                  child: SizedBox(
-                    width: MediaQuery.of(context).size.width *
-                        (clickCount > 0 ? dividerWidth : 0),
-                    child: const Divider(
-                      thickness: 4,
-                      color: Pallete.warning,
-                    ),
+                  const SizedBox(
+                    height: 8,
                   ),
-                ),
-                SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.1,
-                  child: ListView.separated(
-                    separatorBuilder: (context, index) => const SizedBox(
-                      width: 22,
-                    ),
-                    scrollDirection: Axis.horizontal,
-                    itemCount: 7,
-                    itemBuilder: (context, index) => Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Container(
-                          padding: const EdgeInsets.all(8),
-                          decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              color: clickCount > index
-                                  ? Pallete.warning
-                                  : Pallete.dark3),
-                          child: clickCount > index
-                              ? const Icon(
-                                  Icons.check,
-                                  color: Pallete.textMainButton,
-                                  size: 22,
-                                )
-                              : Text(
-                                  "${10 * (index + 1)}",
-                                  style: ThemeFont.interText
-                                      .copyWith(color: Pallete.textMainButton),
-                                ),
+                  Text(
+                    state.data.point.toString(),
+                    style: ThemeFont.interText
+                        .copyWith(fontWeight: FontWeight.w700, fontSize: 28),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Stack(
+                    children: [
+                      Positioned(
+                        top: 13,
+                        child: SizedBox(
+                          width: MediaQuery.of(context).size.width,
+                          child: const Divider(
+                            thickness: 4,
+                          ),
                         ),
-                        Text(
-                          "Hari ${index + 1}",
+                      ),
+                      Positioned(
+                        top: 13,
+                        child: SizedBox(
+                          width: MediaQuery.of(context).size.width *
+                              (clickCount > 0 ? dividerWidth : 0),
+                          child: const Divider(
+                            thickness: 4,
+                            color: Pallete.warning,
+                          ),
                         ),
-                      ],
-                    ),
+                      ),
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.1,
+                        child: ListView.separated(
+                          separatorBuilder: (context, index) => const SizedBox(
+                            width: 22,
+                          ),
+                          scrollDirection: Axis.horizontal,
+                          itemCount: 7,
+                          itemBuilder: (context, index) => Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                padding: const EdgeInsets.all(8),
+                                decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    color: clickCount > index
+                                        ? Pallete.warning
+                                        : Pallete.dark3),
+                                child: clickCount > index
+                                    ? const Icon(
+                                        Icons.check,
+                                        color: Pallete.textMainButton,
+                                        size: 22,
+                                      )
+                                    : Text(
+                                        "${10 * (index + 1)}",
+                                        style: ThemeFont.interText.copyWith(
+                                            color: Pallete.textMainButton),
+                                      ),
+                              ),
+                              Text(
+                                "Hari ${index + 1}",
+                              ),
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
                   ),
-                )
-              ],
-            ),
-            SizedBox(
-                width: MediaQuery.of(context).size.width,
-                child: MainButton(
-                    onPressed: () {
-                      context
-                          .read<PostPoinDailyCubit>()
-                          .claimPoin(context: context);
-                    },
-                    child: Text(
-                      "Kumpulkan",
-                      style: ThemeFont.heading6Reguler.copyWith(
-                          color: Pallete.textMainButton,
-                          fontWeight: FontWeight.w700),
-                    )))
-          ],
-        ),
+                  SizedBox(
+                      width: MediaQuery.of(context).size.width,
+                      child: MainButton(
+                          onPressed: () {
+                            context
+                                .read<PostPoinDailyCubit>()
+                                .claimPoin(context: context);
+                          },
+                          child: Text(
+                            "Kumpulkan",
+                            style: ThemeFont.heading6Reguler.copyWith(
+                                color: Pallete.textMainButton,
+                                fontWeight: FontWeight.w700),
+                          )))
+                ],
+              ),
+            );
+          }
+          return SizedBox();
+        },
       ),
     );
   }
